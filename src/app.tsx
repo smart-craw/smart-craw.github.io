e95c7462cde4474279a53c9e918c85b74ed561a6
Daniel Stahl <danstahl1138@gmail.com>
2026-03-22 16:17:31 -0500

    fixed tests and improved layout

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,45 @@ import smartCrawImg from "./assets/smart_craw_crab_only.svg";
 import smartCrawSvg from "./assets/smart_craw_chonky_crab.svg";
 import screenshot from "./assets/ui.png";
 import "./app.css";
-import { Carousel, Layout, Menu, theme, Image, Typography, Space } from "antd";
+import {
+  Layout,
+  Menu,
+  theme,
+  Image,
+  Typography,
+  Space,
+  Masonry,
+  Card,
+} from "antd";
 import { HomeOutlined, ReadOutlined, SlidersOutlined } from "@ant-design/icons";
 
 const { Header, Sider, Content } = Layout;
-const { Title, Text } = Typography;
+const { Title } = Typography;
+
+const cardData = [
+  {
+    img: smartCrawSvg,
+    title: "Its not Claw, its Craw!",
+    description:
+      "Create, test, and schedule agents. Use agents as an orchestrator, or use an external orchestrator to call agents: regardless of your workflow, Smart Craw has you covered!",
+  },
+  {
+    img: screenshot,
+    title: "Intuitive UI",
+    description: "UI to create, view, schedule, start, and stop bot execution",
+  },
+  {
+    img: screenshot,
+    title: "Test your bots!",
+    description: "Chat interface playground to test and explore",
+  },
+  {
+    img: screenshot,
+    title: "Sandboxed by default",
+    description:
+      "Full control over access: dockerized by default, with explicit volume mounts for workstation data access",
+  },
+];
 export function App() {
   const {
     token: { colorBgContainer, borderRadiusLG },
@@ -50,52 +84,51 @@ export function App() {
                 marginLeft: 16,
               }}
             >
-              Smart Craw
+              Get Smart. Get Smart Craw.
             </Title>
           </Space>
         </Header>
         <Content
           style={{
-            margin: "24px 16px",
-            padding: 24,
+            marginTop: 16,
+            //margin: "24px 16px",
+            //padding: 24,
             minHeight: 280,
             background: colorBgContainer,
             borderRadius: borderRadiusLG,
           }}
         >
-          <Title level={4}>Get Smart! Its not Claw, its Craw!</Title>
-          <Text>
-            Create, test, and schedule agents. Use agents as an orchestrator, or
-            use an external orchestrator to call agents: regardless of your
-            workflow, Smart Craw has you covered!
-          </Text>
-          <Carousel arrows infinite={false}>
-            <div>
-              <Image src={smartCrawSvg} />
-            </div>
-            <div>
-              <Image src={screenshot} />
-              <Text>
-                UI to create, view, schedule, start, and stop bot execution
-              </Text>
-            </div>
-            <div>
-              <Image src={screenshot} />
-              <Text>Chat interface playground to test and explore</Text>
-            </div>
-            <div>
-              <Image src={smartCrawSvg} />
-              <Text>
-                Full control over access: docker-ized by default, with explicit
-                volume mounts for workstation data access
-              </Text>
-            </div>
-          </Carousel>
+          <Masonry
+            columns={{ xs: 1, sm: 1, md: 2, lg: 3, xl: 4 }}
+            //gutter={{ xs: 8, sm: 12, md: 16 }}
+            gutter={0}
+            items={cardData.map((data, index) => ({
+              key: `item-${index}`,
+              data: data,
+            }))}
+            itemRender={({ data }) => (
+              <Card
+                style={{ margin: 16 }}
+                size="small"
+                cover={<img alt="logo" src={data.img} />}
+              >
+                <Card.Meta title={data.title} description={data.description} />
+              </Card>
+            )}
+          />
         </Content>
       </Layout>
     </Layout>
   );
 }
+
+/*<Title level={4}>Get Smart! Its not Claw, its Craw!</Title>
+<Text>
+  Create, test, and schedule agents. Use agents as an orchestrator, or
+  use an external orchestrator to call agents: regardless of your
+  workflow, Smart Craw has you covered!
+</Text> */
+
 /*<Space align="center">
   <Button
     type="text"
@@ -109,3 +142,60 @@ export function App() {
   />
 
 </Space> */
+/*<Carousel arrows infinite={false}>
+  <div>
+    <Image src={smartCrawSvg} />
+  </div>
+  <div>
+    <Image src={screenshot} />
+    <Text>
+      UI to create, view, schedule, start, and stop bot execution
+    </Text>
+  </div>
+  <div>
+    <Image src={screenshot} />
+    <Text>Chat interface playground to test and explore</Text>
+  </div>
+  <div>
+    <Image src={smartCrawSvg} />
+    <Text>
+      Full control over access: docker-ized by default, with explicit
+      volume mounts for workstation data access
+    </Text>
+  </div>
+</Carousel> */
+
+/*<Card size="small" cover={<img alt="logo" src={smartCrawSvg} />}>
+  <Card.Meta
+    title="Get Smart! Its not Claw, its Craw!"
+    description="Create, test, and schedule agents. Use agents as an orchestrator, or use an external orchestrator to call agents: regardless of your workflow, Smart Craw has you covered!"
+  />
+</Card>
+<Card
+  size="small"
+  cover={<img alt="screenshot" src={screenshot} />}
+>
+  <Card.Meta
+    title="Intuitive UI"
+    description="UI to create, view, schedule, start, and stop bot execution"
+  />
+</Card>
+<Card
+  size="small"
+  cover={<img alt="screenshot" src={screenshot} />}
+>
+  <Card.Meta
+    title="Test your bots!"
+    description="Chat interface playground to test and explore"
+  />
+</Card>
+<Card
+  size="small"
+  cover={<img alt="screenshot" src={screenshot} />}
+>
+  <Card.Meta
+    title="Sandboxed by default"
+    description="Full control over access: docker-ized by default, with explicit volume mounts for workstation data access"
+  />
+</Card>
+</Masonry> */
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-
+import { defineConfig } from "vitest/config";
+import preact from "@preact/preset-vite";
+import { playwright } from "@vitest/browser-playwright";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright(),
+      instances: [{ browser: "chromium" }],
+    },
+    coverage: {
+      include: ["src"],
+    },
+  },
+});
